0a5945f7cb37eeb5088671810d1c6b429d316f87
MonikySouzaLopes <monikysouzalopes@gmail.com>
2023-05-24 09:40:06 -0300

    Adicionado CSS

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ class Produto {
 }
 
 mostrar_produtos(){
- return this.nome + this.Datacadastro + this.descricao + this.preco
-}
+ return    `
+  <div class= "descricao"> <strong>${this.nome}</strong></div>
+ <div class= "descricao">  Data de cadastro: ${this.Datacadastro}</div>
+ <div class= "descricao"> Descrição: ${this.descricao}</div>
+ <div class= "descricao"> <strong>R$${this.preco},00</strong></div>
+<br>
+`}
 
 }
 
@@ -22,25 +27,36 @@ this.img = img;
 mostrar_produtos_destaque(){
     return `
            <center><img src= "${this.img}"/></center>
-           <div class= "descricao"> ${this.nome}</div>
-           <div class= "descricao">  Data de cadastro :${this.Datacadastro}</div>
-           <div class= "descricao"> Descrição ${this.descricao}</div>
-           <div class= "descricao"> R$${this.preco},00</div>
+           <div class= "descricao"> <strong>${this.nome}</strong></div>
+           <div class= "descricao">  Data de cadastro: ${this.Datacadastro}</div>
+           <div class= "descricao"> Descrição: ${this.descricao}</div>
+           <div class= "descricao"> <strong>R$${this.preco},00</strong></div>
            `
 }
 
 }
-const produto = new Produto("Secador","10/08/2022","seca cabelo",300)
-console.log(produto.mostrar_produtos())
+
  
 const produtoDestaque = new ProdutoDestaque("Modelador de cachos","10/08/2022","Modelador de Cachos Mondial, Spiral Infinity, Bivolt, Preto/Vermelho - EM-05",250,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2NHE97lNTWvaTCY1U_kCa82Y438PLU_e1jg&usqp=CAU")
 console.log(produtoDestaque.mostrar_produtos_destaque())
 
-const produtoLista = new ProdutoDestaque("Secador de cabelo","10/08/2022","Modelador de Cachos Mondial, Spiral Infinity, Bivolt, Preto/Vermelho - EM-05",250,"https://a-static.mlcdn.com.br/800x560/secador-de-cabelo-profissional-fox-ion-prata-2-000w-taiff/mebabocom/0247-secadorfox220v-01/dd950697e92abcd3e69fb6a05e9f357f.jpeg")
-console.log(produtoDestaque.mostrar_produtos_destaque())
+const produtoLista = new Produto("Secador de cabelo","10/10/2022","Secador de Cabelo Profissional Fox Íon Prata 2.000W Taiff",486)
+console.log(produtoLista.mostrar_produtos())
+
+const produtoLista2 = new Produto("Chapinha","22/08/2022","Chapinha/Prancha de Cabelo Philips Essential - BHS378/00 Cerâmica e com Íons 230C",155)
+console.log(produtoLista2.mostrar_produtos())
+
+const produtoLista3 = new Produto("Escova Alisadora ","15/05/2022","Escova Alisadora Britânia Soft Bec07r Biv 1300w Profissional",857)
+console.log(produtoLista3.mostrar_produtos())
 
 const div = document.getElementById("produto-destaque")
 div.insertAdjacentHTML('afterbegin', produtoDestaque.mostrar_produtos_destaque());
 
-const div1 = document.getElementById("lista-produtos")
-div.insertAdjacentHTML('afterbegin', produtoLista.mostrar_produtos_destaque());
+const div1 = document.getElementById("lista-produto")
+div1.insertAdjacentHTML('afterbegin', produtoLista.mostrar_produtos());
+
+const div2 = document.getElementById("lista-produto")
+div2.insertAdjacentHTML('afterbegin', produtoLista2.mostrar_produtos());
+
+const div3 = document.getElementById("lista-produto")
+div3.insertAdjacentHTML('afterbegin', produtoLista3.mostrar_produtos());
